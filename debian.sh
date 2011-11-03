7b5328d0e79a7c504038e78a4099661bc3c70bfa
eyealike <hannes@eyealike.com>
2011-11-02 19:40:20 -0700

    Added Debian build script for inhouse builds

---
+++ debian.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ex
+
+psPlatformVersion="3.3.0"
+distribution="lucid"
+upstreamVersion="0.4.0"
+debianVersion="0ubuntu${psPlatformVersion}"
+
+config/autorun.sh
+
+dch \
+	--distribution "${distribution}" \
+	--force-distribution \
+	--newversion "${upstreamVersion}-${debianVersion}~${BUILD_NUMBER}" \
+	"Continuous integration build #${BUILD_NUMBER}"
+
+debuild -i -us -uc -b
