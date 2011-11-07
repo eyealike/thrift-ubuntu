bd37aff780086016f4d0dea81ca8c5d0086de336
eyealike <hannes@eyealike.com>
2011-11-07 15:55:31 -0800

    Tweaked Debian versioning

--- debian.sh
+++ debian.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -ex
 
-psPlatformVersion="3.4.0"
+psPlatformVersion="ps3.4.0"
 distribution="lucid"
 upstreamVersion="0.6.1"
-debianVersion="0ubuntu${psPlatformVersion}"
+debianVersion="0ubuntu1"
 
 dch \
 	--distribution "${distribution}" \
 	--force-distribution \
-	--newversion "${upstreamVersion}-${debianVersion}~${BUILD_NUMBER}" \
+	--newversion "${upstreamVersion}-${debianVersion}-${psPlatformVersion}~${BUILD_NUMBER}" \
 	"Continuous integration build #${BUILD_NUMBER}"
 
 debuild -i -us -uc -b
